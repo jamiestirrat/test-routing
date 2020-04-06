69521a2bc14dacab739bdfe552fd26b0d11818dd
James Stirrat <jamiestirrat@hotmail.co.uk>
2020-04-06 23:53:40 +0100

    first pass navigation between two screens

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppNavigator from './AppNavigator';
+import AppContainer from './AppNavigator';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppContainer/>
+    );
+  }
+}
---
+++ AppNavigator.js
@@ -0,0 +1,12 @@
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import Home from './Home';
+import Friends from './Friends';
+
+const AppNavigator = createStackNavigator({
+  Home: { screen: Home },
+  Friends: { screen: Friends },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+export default AppContainer;
